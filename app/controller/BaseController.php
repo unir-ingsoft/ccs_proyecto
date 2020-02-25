78047afeac678c3df09ff35561e76a268b94108e
Enrique Lopez <enrique.lopez@deltascript.com>
2020-02-25 13:52:24 -0700

    logout

--- app/controller/BaseController.php
+++ app/controller/BaseController.php
@@ -7,3 +7,11 @@ $app->get('/registro/', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'registro.phtml', ["error" => ""]);
 });
+
+$app->get('/logout/', function ($request, $response, $args) {
+    // Render index view
+    unset($_SESSION['logged']);
+    unset($_SESSION['nombre']);
+    unset($_SESSION['usuariopk']);
+    return $this->renderer->render($response, 'index.phtml', ["error" => ""])->withHeader('Location', 'http://kornmexico.com/unirlab02/v1/');
+});
